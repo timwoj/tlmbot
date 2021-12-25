747ddf7076a127251b5a5b73c9986e2c1824d1ee
Tim Wojtulewicz <timwoj@gmail.com>
2021-12-25 16:33:26 -0700

    db_utils: Add karma methods, add unit test scaffolding

--- db_utils.py
+++ db_utils.py
@@ -1,12 +1,20 @@
+import os
 import sqlite3
+import sys
+import unittest
+
 from furl import furl
 from urllib.parse import urlparse,parse_qs
 from datetime import datetime
 
 def connect(path):
-    conn = sqlite3.connect(path)
-    if not conn:
-        print('Failed to load database file \'{:s}\''.format(config_file['database_file']))
+
+    conn = None
+    try:
+        conn = sqlite3.connect(path)
+    except:
+        if 'unittest' not in sys.modules.keys():
+            print(f'Failed to load database file \'{path}\': {sys.exc_info()[1]}')
         return None
 
     # Ensure we get Row objects back for queries. This makes handling
@@ -16,10 +24,15 @@ def connect(path):
     # Create a cursor and add the table if it doesn't exist already.
     cur = conn.cursor()
     cur.execute('create table if not exists urls('
-                'url text primary key not null,'
-                'count integer default 1,'
-                'latest datetime not null default current_timestamp,'
-                'orig_paster text not null,'
+                'url text primary key not null, '
+                'count integer default 1, '
+                'latest datetime not null default current_timestamp, '
+                'orig_paster text not null, '
+                'orig_date datetime not null default current_timestamp)')
+    cur.execute('create table if not exists karma('
+                'string text primary key not null, '
+                'count integer default 1, '
+                'orig_paster text not null, '
                 'orig_date datetime not null default current_timestamp)')
     conn.commit()
     return conn
@@ -70,5 +83,121 @@ def store_url(db, url, paster):
 
         return None
 
-def get_urls(db, start_date, end_date, pattern):
-    print('get_urls')
+def get_urls(db, start_date=None, end_date=None, pattern=None):
+
+    cur = db.cursor()
+    command = 'select * from urls'
+    stack = []
+
+    if start_date:
+        command += ' where latest >= ?'
+        stack.append(start_date)
+
+    if end_date:
+        if command.find('where') == -1:
+            command += ' where'
+        command += ' latest <= ?'
+        stack.append(end_date)
+
+    if pattern:
+        if command.find('where') == -1:
+            command += ' where'
+        command += ' url like ?'
+        stack.append(f'%{pattern}%')
+
+    command += ' order by latest desc'
+
+    cur.execute(command, stack)
+    results = cur.fetchall()
+    ret = []
+    for r in results:
+        ret.append({'url': results['url'],
+                    'count': results['count'],
+                    'when': results['latest']})
+
+    return ret
+
+def set_karma(db, text, paster, increase):
+    cur = db.cursor()
+    cur.execute('select * from karma where string = ?', [text])
+    results = cur.fetchone()
+
+    if results:
+        count = int(results['count'])
+        if increase:
+            count += 1
+        else:
+            count -= 1
+        cur.execute('update karma set count = ? where string = ?',
+                    [count, text])
+    else:
+        cur.execute('insert into karma (string, orig_paster) values(?, ?14)',
+                    [text, paster])
+
+    db.commit()
+
+def get_karma(db, text):
+    cur = db.cursor()
+    cur.execute('select * from karma where string = ?', [text])
+    results = cur.fetchone()
+
+    count = 0
+    if results:
+        count = int(results['count'])
+
+    return count
+
+class _TestCases(unittest.TestCase):
+
+    def test_connect_failure(self):
+        db = connect('/tmp/dbutil-test/sqlite.db')
+        self.assertEqual(db, None)
+
+    def test_connect_success(self):
+        import tempfile
+        path = os.path.join(tempfile.gettempdir(), 'testing-dbutils-sqlite.db')
+        db = connect(str(path))
+        self.assertNotEqual(db, None)
+        db.close()
+        os.unlink(str(path))
+
+    # TODO: decide how best to test the rest of these
+    def test_store_url(self):
+        import tempfile
+        path = os.path.join(tempfile.gettempdir(), 'testing-dbutils-sqlite.db')
+        db = connect(str(path))
+
+        db.close()
+        os.unlink(str(path))
+
+        return
+
+    def test_get_urls(self):
+        import tempfile
+        path = os.path.join(tempfile.gettempdir(), 'testing-dbutils-sqlite.db')
+        db = connect(str(path))
+
+        db.close()
+        os.unlink(str(path))
+        return
+
+    def test_set_karma(self):
+        import tempfile
+        path = os.path.join(tempfile.gettempdir(), 'testing-dbutils-sqlite.db')
+        db = connect(str(path))
+
+        db.close()
+        os.unlink(str(path))
+        return
+
+    def test_get_karma(self):
+        import tempfile
+        path = os.path.join(tempfile.gettempdir(), 'testing-dbutils-sqlite.db')
+        db = connect(str(path))
+
+        db.close()
+        os.unlink(str(path))
+        return
+
+if __name__ == "__main__":
+    unittest.main()
