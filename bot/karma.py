d1748f63f7481e3b3f9321b97faf91807df006e3
Tim Wojtulewicz <timwoj@gmail.com>
2022-01-08 10:28:01 -0700

    bot: Replace karma lookup with command

--- bot/karma.py
+++ bot/karma.py
@@ -9,12 +9,11 @@ class Karma(commands.Cog, name='Karma'):
     """
     Karma module
 
-    Stores "karma" for words or underscore-separated strings. For example, you can say 'something++' to give the word 'something' some karma. Similarly, you can use 'something--' to remove karma. Multiple words should be separated by underscores like 'something_else'. You can look up karma but simply saying 'karma something'.
+    Stores "karma" for words or underscore-separated strings. For example, you can say 'something++' to give the word 'something' some karma. Similarly, you can use 'something--' to remove karma. Multiple words should be separated by underscores like 'something_else'. The !karma command can be used to look up the current value.
     """
 
     karma_add_re = re.compile('(.*)\+\+$', re.IGNORECASE | re.DOTALL)
     karma_subtract_re = re.compile('(.*)--$', re.IGNORECASE | re.DOTALL)
-    karma_lookup_re = re.compile('^karma (.*)', re.IGNORECASE | re.DOTALL)
 
     def __init__(self, bot):
         self.bot = bot
@@ -24,12 +23,7 @@ class Karma(commands.Cog, name='Karma'):
         if bot_utils.should_ignore(message, self.bot):
             return
 
-        if (m := re.match(self.karma_lookup_re, message.content)) is not None:
-            text = m.group(1)
-            res = db_utils.get_karma(self.bot.db, text.lower())
-            await message.reply(f'{text} has a karma of {res}')
-
-        elif (m := re.match(self.karma_add_re, message.content)) is not None:
+        if (m := re.match(self.karma_add_re, message.content)) is not None:
             text = m.group(1)
             space_pos = text.rfind(' ')
             if space_pos != -1:
@@ -42,3 +36,16 @@ class Karma(commands.Cog, name='Karma'):
             if space_pos != -1:
                 text = text[space_pos:]
             db_utils.set_karma(self.bot.db, text.lower(), message.author.name, False)
+
+    @commands.command()
+    async def karma(self, ctx, text=None):
+        """
+        Looks up the karma for a word or underscore-separated string.
+        For example, !karma something or !karma something_something
+        """
+        if not text:
+            await ctx.reply(f'Did you mean to put some text to get karma for? Try !help karma')
+            return
+
+        res = db_utils.get_karma(self.bot.db, text.lower())
+        await ctx.reply(f'{text} has a karma of {res}')
